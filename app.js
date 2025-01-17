c7d4313c913ec16657f9725165d5ea03f9f2faba
surya20000 <surya.p@kalvium.community>
2025-01-17 14:09:14 +0530

    Implemented/ Initial game part

---
+++ app.js
@@ -0,0 +1,14 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+const logsLeft = document.querySelectorAll('.log-left')
+const logsRight = document.querySelectorAll('.log-right')
+const carsLeft = document.querySelectorAll('.car-left')
+const carsRight = document.querySelectorAll('.car-right')
+
+let currentIndex = 76
+const width = 9
+let timerId
+let outcomeTimerId
+let currentTime = 20
